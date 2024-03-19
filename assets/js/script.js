12df801b2fc9495ac69d1961d692c888e437c4d4
annaferrari8 <156824038+annaferrari8@users.noreply.github.com>
2024-03-19 16:24:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,58 @@
-//body
-const wrapper = document.createElement("div");
-wrapper.className = "pageWrapper";
-document.body.appendChild(wrapper);
+//body 
+
+inizializer_function("wrapper","div",document.body,"pageWrapper");
 
 // counter
-const counterContainer = document.createElement("section");
-counterContainer.className = "counter_container"
-wrapper.appendChild(counterContainer);
+inizializer_function("counterContainer","section",wrapper,"counter_container");
+
 
 // display of the counter
-const displayContainer = document.createElement("section");
-displayContainer.className = "display_container";
-counterContainer.appendChild(displayContainer);
-const displayTitle = document.createElement("h2");
-displayTitle.id = "display";
-displayTitle.textContent = "0";
-displayContainer.appendChild(displayTitle);
+inizializer_function("displayContainer","section",counterContainer,"display_container");
+inizializer_function("displayTitle","h2",displayContainer,"display_container","display","0");
+
+// variabili
 let counter = 0;
 let display = document.getElementById("display");
 
 // button wrapper
-const buttonContainer = document.createElement("section");
-buttonContainer.className = "button_container";
-counterContainer.appendChild(buttonContainer);
+inizializer_function("buttonContainer","section",counterContainer,"button_container");
+
 
 // +, -, reset
+inizializer_function("decrementButton","div",buttonContainer,"decrement_div");
+inizializer_function("incrementButton","div",buttonContainer,"increment_div");
+inizializer_function("resetButton","div",buttonContainer,"reset_div");
+inizializer_function("buttonmeno","button",decrementButton,"decrement_button");
 
-const decrementButton = document.createElement("div");
-const incrementButton = document.createElement("div");
-const resetButton = document.createElement("div");
-buttonContainer.appendChild(decrementButton);
-buttonContainer.appendChild(incrementButton);
-buttonContainer.appendChild(resetButton);
+// creazione bottoni
+createButton(buttonmeno,"-", change);
+inizializer_function("buttonpiu","button",incrementButton,"increment_button");
+createButton(buttonpiu,"+", change);
+inizializer_function("buttonstartover","button",resetButton,"reset_button");
+createButton(buttonstartover,"Start Over", change);
 
-let buttonmeno = createButton("-", change);
-let buttonpiu = createButton("+", change);
-let buttonstartover = createButton("Start Over", change);
-decrementButton.appendChild(buttonmeno);
-incrementButton.appendChild(buttonpiu);
-resetButton.appendChild(buttonstartover);
 
 // functions
-function createButton(text, event) {
-    let button = document.createElement("button");
+function createButton(button, text, event) {
     button.innerHTML = text;
     if(event) {
         button.addEventListener("click", event);
     }
     return button;
+}
+
+//inizializer
+
+function inizializer_function(varName,element,appendToObj,className,id = "",content = "",callback = null){
+	window[varName] = document.createElement(element);
+  window[varName].className = className;
+  if(id != ""){
+  	displayTitle.id = id;
+  }
+  if(content != ""){
+  	displayTitle.textContent = content;
+  }
+	appendToObj.appendChild(window[varName]);
 }
 
 function change(option){
